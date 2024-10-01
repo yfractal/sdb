c8387a7fc5636a788c0576e97684109b7e54a213
Mike Yang <yfractal@gmail.com>
2024-10-01 10:02:12 +0800

    feat: clean symbolizer

--- symbolizer/symbolizer.py
+++ symbolizer/symbolizer.py
@@ -2,10 +2,15 @@ from bcc import BPF
 import ctypes
 import json
 
+MAX_STR_LENGTH = 128
+
 bpf_text = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
 
+#define MAX_STR_LENGTH 128
+
+// for ruby 3.1.5 only
 typedef unsigned long VALUE;
 
 struct RBasic {
@@ -37,8 +42,8 @@ struct event_t {
     u32 tid;
     u64 ts;
     u32 first_lineno;
-    char name[128];
-    char path[128];
+    char name[MAX_STR_LENGTH];
+    char path[MAX_STR_LENGTH];
     u64 iseq_addr;
     u32 debug;
 };
@@ -77,21 +82,19 @@ static inline int read_rstring(struct RString *str, char *buff) {
 
         return 1;
     } else {
-        bpf_trace_printk("branch 2", sizeof("branch 2"));
-        int len = get_embed_ary_len(str->as.embed.ary, 128);
+        int len = get_embed_ary_len(str->as.embed.ary, MAX_STR_LENGTH);
+        // 0x7F is 127
         bpf_probe_read_str(buff, (len &= 0x7F) + 1, str->as.embed.ary);
 
         return 2;
     }
 }
 
-// ruby 3.1.5
 // rb_iseq_t *
 // rb_iseq_new_with_opt(const rb_ast_body_t *ast, VALUE name, VALUE path, VALUE realpath,
 //                      VALUE first_lineno, const rb_iseq_t *parent, int isolated_depth,
 //                      enum iseq_type type, const rb_compile_option_t *option)
 int rb_iseq_new_with_opt_instrument(struct pt_regs *ctx) {
-    int one = 1;
     u64 pid_tgid = bpf_get_current_pid_tgid();
     u64 pid = pid_tgid >> 32;
     u64 tid = pid_tgid & 0xFFFFFFFF;
@@ -108,8 +111,7 @@ int rb_iseq_new_with_opt_instrument(struct pt_regs *ctx) {
 
     struct RString *name;
     bpf_probe_read(&name, sizeof(name), (void *)&PT_REGS_PARM2(ctx));
-    int i = read_rstring(name, event.name);
-    event.debug = i;
+    read_rstring(name, event.name);
 
     struct RString *path;
     bpf_probe_read(&path, sizeof(path), (void *)&PT_REGS_PARM3(ctx));
@@ -120,7 +122,7 @@ int rb_iseq_new_with_opt_instrument(struct pt_regs *ctx) {
     return 0;
 }
 
-int rb_iseq_new_with_opt_return(struct pt_regs *ctx) {
+int rb_iseq_new_with_opt_return_instrument(struct pt_regs *ctx) {
     u64 pid_tgid = bpf_get_current_pid_tgid();
     u64 ret_val = PT_REGS_RC(ctx);
     struct event_t *event = events_map.lookup(&pid_tgid);
@@ -135,14 +137,10 @@ int rb_iseq_new_with_opt_return(struct pt_regs *ctx) {
 }
 """
 
-
 b = BPF(text=bpf_text)
-
 binary_path = "/home/ec2-user/.rvm/rubies/ruby-3.1.5/lib/libruby.so.3.1"
-
-# TODO: probe other methods
 b.attach_uprobe(name=binary_path, sym="rb_iseq_new_with_opt", fn_name="rb_iseq_new_with_opt_instrument")
-b.attach_uretprobe(name=binary_path, sym="rb_iseq_new_with_opt", fn_name="rb_iseq_new_with_opt_return")
+b.attach_uretprobe(name=binary_path, sym="rb_iseq_new_with_opt", fn_name="rb_iseq_new_with_opt_return_instrument")
 
 class Event(ctypes.Structure):
     _fields_ = [
@@ -150,10 +148,10 @@ class Event(ctypes.Structure):
         ("tid", ctypes.c_uint32),
         ("ts", ctypes.c_uint64),
         ("first_lineno", ctypes.c_uint32),
-        ("name", ctypes.c_char * 128),
-        ("path", ctypes.c_char * 128),
-        ("debug", ctypes.c_uint32),
+        ("name", ctypes.c_char * MAX_STR_LENGTH),
+        ("path", ctypes.c_char * MAX_STR_LENGTH),
         ("iseq_addr", ctypes.c_uint64),
+        ("debug", ctypes.c_uint32),
     ]
 
     def to_dict(self):
