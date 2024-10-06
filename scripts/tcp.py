6134725f04fbff1efdf9cb3facdc1a7b04c93c54
Mike Yang <yfractal@gmail.com>
2024-10-06 11:00:50 +0800

    feat: remove unnecessary fileds

--- scripts/tcp.py
+++ scripts/tcp.py
@@ -1,8 +1,9 @@
-from __future__ import print_function
 from bcc import BPF
 from socket import inet_ntop, AF_INET, AF_INET6
 from struct import pack
 import argparse
+
+# inspired by bcc tcpconnlat
 
 # arguments
 examples = """examples:
@@ -29,34 +30,29 @@ bpf_text = """
 struct info_t {
     u64 ts;
     u32 pid;
-    char task[TASK_COMM_LEN];
 };
 BPF_HASH(start, struct sock *, struct info_t);
 
 // separate data structs for ipv4 and ipv6
 struct ipv4_data_t {
-    u64 ts_us;
     u32 pid;
-    u32 saddr;
     u32 daddr;
     u64 ip;
     u16 lport;
     u16 dport;
-    u64 delta_us;
-    char task[TASK_COMM_LEN];
+    u64 start_ts;
+    u64 end_ts;
 };
 BPF_PERF_OUTPUT(ipv4_events);
 
 struct ipv6_data_t {
-    u64 ts_us;
     u32 pid;
-    unsigned __int128 saddr;
     unsigned __int128 daddr;
     u64 ip;
     u16 lport;
     u16 dport;
-    u64 delta_us;
-    char task[TASK_COMM_LEN];
+    u64 start_ts;
+    u64 end_ts;
 };
 BPF_PERF_OUTPUT(ipv6_events);
 
@@ -64,21 +60,15 @@ int trace_tcp_connect(struct pt_regs *ctx, struct sock *sk)
 {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     FILTER
-    struct info_t info = {.pid = pid};
+    struct info_t info = {};
+    info.pid = pid;
     info.ts = bpf_ktime_get_ns();
-    bpf_get_current_comm(&info.task, sizeof(info.task));
     start.update(&sk, &info);
     return 0;
 };
 
-// See tcp_v4_do_rcv() and tcp_v6_do_rcv(). So TCP_ESTBALISHED and TCP_LISTEN
-// are fast path and processed elsewhere, and leftovers are processed by
-// tcp_rcv_state_process(). We can trace this for handshake completion.
-// This should all be switched to static tracepoints when available.
 int trace_tcp_close(struct pt_regs *ctx, struct sock *skp)
 {
-    bpf_trace_printk("tcp_close");
-    // check start and calculate delta
     struct info_t *infop = start.lookup(&skp);
     if (infop == 0) {
         return 0;   // missed entry or filtered
@@ -87,9 +77,6 @@ int trace_tcp_close(struct pt_regs *ctx, struct sock *skp)
     u64 ts = infop->ts;
     u64 now = bpf_ktime_get_ns();
 
-    u64 delta_us = (now - ts) / 1000ul;
-
-    // pull in details
     u16 family = 0, lport = 0, dport = 0;
     family = skp->__sk_common.skc_family;
     lport = skp->__sk_common.skc_num;
@@ -97,27 +84,26 @@ int trace_tcp_close(struct pt_regs *ctx, struct sock *skp)
 
     // emit to appropriate data path
     if (family == AF_INET) {
-        struct ipv4_data_t data4 = {.pid = infop->pid, .ip = 4};
-        data4.ts_us = now / 1000;
-        data4.saddr = skp->__sk_common.skc_rcv_saddr;
+        struct ipv4_data_t data4 = {};
+        data4.pid = infop->pid;
+        data4.ip = 4;
         data4.daddr = skp->__sk_common.skc_daddr;
         data4.lport = lport;
         data4.dport = ntohs(dport);
-        data4.delta_us = delta_us;
-        __builtin_memcpy(&data4.task, infop->task, sizeof(data4.task));
+        data4.start_ts = ts;
+        data4.end_ts = now;
         ipv4_events.perf_submit(ctx, &data4, sizeof(data4));
 
     } else /* AF_INET6 */ {
-        struct ipv6_data_t data6 = {.pid = infop->pid, .ip = 6};
-        data6.ts_us = now / 1000;
-        bpf_probe_read_kernel(&data6.saddr, sizeof(data6.saddr),
-            skp->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+        struct ipv6_data_t data6 = {};
+        data6.pid = infop->pid;
+        data6.ip = 6;
         bpf_probe_read_kernel(&data6.daddr, sizeof(data6.daddr),
             skp->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
         data6.lport = lport;
         data6.dport = ntohs(dport);
-        data6.delta_us = delta_us;
-        __builtin_memcpy(&data6.task, infop->task, sizeof(data6.task));
+        data6.start_ts = ts;
+        data6.end_ts = now;
         ipv6_events.perf_submit(ctx, &data6, sizeof(data6));
     }
 
@@ -148,22 +134,22 @@ b.attach_kprobe(event="tcp_close", fn_name="trace_tcp_close")
 # process event
 def print_ipv4_event(cpu, data, size):
     event = b["ipv4_events"].event(data)
-    print("%-7d %-12.12s %-2d %-16s %-6d %-16s %-5d %.2f" % (event.pid,
-        event.task.decode('utf-8', 'replace'), event.ip,
-        inet_ntop(AF_INET, pack("I", event.saddr)), event.lport,
+    print("%-12.12s %-2d %-6d %-16s %-5d %d %d" % (event.pid,
+        event.ip,
+        event.lport,
         inet_ntop(AF_INET, pack("I", event.daddr)), event.dport,
-        float(event.delta_us) / 1000))
+        event.start_ts, event.end_ts))
 
 def print_ipv6_event(cpu, data, size):
     event = b["ipv6_events"].event(data)
-    print("%-7d %-12.12s %-2d %-16s %-6d %-16s %-5d %.2f" % (event.pid,
-        event.task.decode('utf-8', 'replace'), event.ip,
-        inet_ntop(AF_INET6, event.saddr), event.lport,
+    print("%-12.12s %-2d %-6d %-16s %-5d %d %d" % (event.pid,
+        event.ip,
+        event.lport,
         inet_ntop(AF_INET6, event.daddr),
-        event.dport, float(event.delta_us) / 1000))
+        event.dport, event.start_ts, event.end_ts))
 
 # header
-print("%-7s %-12s %-2s %-16s %-6s %-16s %-5s %s" % ("PID", "COMM", "IP", "SADDR", "LPORT", "DADDR", "DPORT", "LAT(ms)"))
+print("%-7s %-2s %-16s %-6s %-16s %-5s %s" % ("PID", "IP", "SADDR", "LPORT", "DADDR", "DPORT", "LAT(ms)"))
 
 # read events
 b["ipv4_events"].open_perf_buffer(print_ipv4_event)
