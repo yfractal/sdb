366090a528a1a42420ca6bcf69d983e0714e7f21
Mike Yang <yfractal@gmail.com>
2024-10-13 18:51:40 +0800

    fix: fix typo

--- lib/sdb/puma_patch.rb
+++ lib/sdb/puma_patch.rb
@@ -7,10 +7,11 @@ module Sdb
     module HandleRequest
       def handle_request(client, requests)
         t0 = Time.now
-        Sdb.set_trace_id(Thread.current, client.env['HTTP_TRACE_ID'].to_i)
+        trace_id = client.env['HTTP_TRACE_ID'].to_i
+        Sdb.set_trace_id(Thread.current, trace_id)
         rv = super
         t1 = Time.now
-        puts "trcace_id=#{client.env['HTTP_TRACE_ID'].to_i}, thread_id=#{Thread.current.native_thread_id}, remote_port=#{client.io.peeraddr[1]}, #{(t1 - t0) * 1000} ms"
+        puts "trace_id=#{trace_id}, thread_id=#{Thread.current.native_thread_id}, remote_port=#{client.io.peeraddr[1]}, #{(t1 - t0) * 1000} ms"
 
         rv
       ensure
