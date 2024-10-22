ab74780b40c25ea16463b760431ee63419b08809
Mike Yang <yfractal@gmail.com>
2024-10-22 09:05:02 +0800

    feat: tag status

--- lib/sdb/puma_patch.rb
+++ lib/sdb/puma_patch.rb
@@ -17,14 +17,22 @@ module Sdb
         cpu_time0 = CPUTime.time
         trace_id = client.env['HTTP_TRACE_ID'].to_i
         Sdb.set_trace_id(Thread.current, trace_id)
+        Thread.current[:sdb] = {}
         rv = super
         t1 = Time.now
         cpu_time1 = CPUTime.time
-        Sdb::PumaPatch.logger.info "[SDB][puma-delay]: trace_id=#{trace_id}, thread_id=#{Thread.current.native_thread_id}, remote_port=#{client.io.peeraddr[1]}, delay=#{(t1 - t0) * 1000} ms, cpu_time=#{(cpu_time1 - cpu_time0) * 1000 } ms"
+        Sdb::PumaPatch.logger.info "[SDB][puma-delay]: trace_id=#{trace_id}, thread_id=#{Thread.current.native_thread_id}, remote_port=#{client.io.peeraddr[1]}, start_ts=#{t0.to_f * 1_000_000}, end_ts=#{t1.to_f * 1_000_000}, delay=#{(t1 - t0) * 1000} ms, cpu_time=#{(cpu_time1 - cpu_time0) * 1000 } ms, status=#{Thread.current[:sdb][:status]}"
 
         rv
       ensure
         Sdb.set_trace_id(Thread.current, 0)
+        Thread.current[:sdb] = {}
+      end
+
+      def prepare_response(status, headers, res_body, requests, client)
+        Thread.current[:sdb][:status] = status
+
+        super
       end
     end
   end
