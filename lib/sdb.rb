42721648fcc4603802089624c3d223413b8fd89e
Mike Yang <yfractal@gmail.com>
2024-12-21 11:36:06 +0800

    feat: wrap around thread

--- lib/sdb.rb
+++ lib/sdb.rb
@@ -6,6 +6,23 @@ require_relative "sdb/puma_patch"
 require_relative "sdb/thread_patch"
 
 module Sdb
+  module ThreadInitializePatch
+    def initialize(*args, &block)
+      old_block = block
+
+      block = ->() do
+        puts "before thread start"
+        result = old_block.call(*args)
+        puts "before thread finish"
+        result
+      end
+
+      super(&block)
+    end
+  end
+
+  Thread.prepend(ThreadInitializePatch)
+
   class << self
     def fetch_puma_threads
       # keep a reference as puller runs without gvl
