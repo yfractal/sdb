172a9970d23a3fc3cb4af6bd18951542bbdf80ca
Mike Yang <yfractal@gmail.com>
2025-02-26 09:06:53 +0800

    feat: added benchmark results

--- examples/benchmark.rb
+++ examples/benchmark.rb
@@ -14,6 +14,26 @@ def test(stacks_depth, n)
 end
 
 # sleep_interval = 0.001
-sleep_interval = 0
+# sleep_interval = 0.0_001
+# sleep_interval = 0.00_001
+# sleep_interval = 0.000_001
+sleep_interval = 0.0000_0_01
 Sdb.scan_threads([Thread.current], sleep_interval)
 test(150, 500_000_000)
+
+
+# base line
+# Takes = 7.874596155
+#
+# 0.001 ns
+# sleep interval 1000 ns
+#
+# Takes = 7.872404624
+# sleep interval 10 ns
+# Takes = 7.884911666
+#
+# sleep interval 1 ns
+# Takes = 7.905451317
+#
+# sleep interval 0 ns
+# Takes = 7.889088806
