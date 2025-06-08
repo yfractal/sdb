1ded83475f94481a3e8c0fe90f2ed1bf4a69b70f
Mike Yang <yfractal@gmail.com>
2025-06-08 16:01:09 +0800

    feat: more ruby versions

--- ext/sdb/src/ruby_version.rs
+++ ext/sdb/src/ruby_version.rs
@@ -155,11 +155,37 @@ macro_rules! impl_control_frame_functions {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum RubyVersion {
+    // Ruby 3.1.x
     Ruby310,
+    Ruby311,
+    Ruby312,
+    Ruby313,
+    Ruby314,
     Ruby315,
+    Ruby316,
+    Ruby317,
+
+    // Ruby 3.2.x
     Ruby320,
+    Ruby321,
     Ruby322,
+    Ruby323,
+    Ruby324,
+    Ruby325,
+    Ruby326,
+    Ruby327,
+    Ruby328,
+
+    // Ruby 3.3.x
     Ruby330,
+    Ruby331,
+    Ruby332,
+    Ruby333,
+    Ruby334,
+    Ruby335,
+    Ruby336,
+    Ruby337,
+    Ruby338,
 }
 
 pub trait RubyApiCompat: Send + Sync {
@@ -174,69 +200,141 @@ pub trait RubyApiCompat: Send + Sync {
     unsafe fn iterate_frame_iseqs(&self, ec_val: VALUE, frame_handler: &mut dyn FnMut(u64));
 }
 
-pub struct Ruby310;
+// Macro to reduce duplication for Ruby version implementations
+macro_rules! impl_ruby_version {
+    // Ruby 3.1.x
+    (Ruby310) => {
+        impl_ruby_version_with_module!(Ruby310, ruby_3_1_0);
+    };
+    (Ruby311) => {
+        impl_ruby_version_with_module!(Ruby311, ruby_3_1_1);
+    };
+    (Ruby312) => {
+        impl_ruby_version_with_module!(Ruby312, ruby_3_1_2);
+    };
+    (Ruby313) => {
+        impl_ruby_version_with_module!(Ruby313, ruby_3_1_3);
+    };
+    (Ruby314) => {
+        impl_ruby_version_with_module!(Ruby314, ruby_3_1_4);
+    };
+    (Ruby315) => {
+        impl_ruby_version_with_module!(Ruby315, ruby_3_1_5);
+    };
+    (Ruby316) => {
+        impl_ruby_version_with_module!(Ruby316, ruby_3_1_6);
+    };
+    (Ruby317) => {
+        impl_ruby_version_with_module!(Ruby317, ruby_3_1_7);
+    };
 
-impl RubyApiCompat for Ruby310 {
-    impl_iseq_functions!(rbspy_ruby_structs::ruby_3_1_0::rb_iseq_struct);
-    impl_thread_functions!(rbspy_ruby_structs::ruby_3_1_0::rb_thread_t);
-    impl_control_frame_functions!(
-        rbspy_ruby_structs::ruby_3_1_0::rb_control_frame_struct,
-        rbspy_ruby_structs::ruby_3_1_0::rb_execution_context_struct
-    );
+    // Ruby 3.2.x
+    (Ruby320) => {
+        impl_ruby_version_with_module!(Ruby320, ruby_3_2_0);
+    };
+    (Ruby321) => {
+        impl_ruby_version_with_module!(Ruby321, ruby_3_2_1);
+    };
+    (Ruby322) => {
+        impl_ruby_version_with_module!(Ruby322, ruby_3_2_2);
+    };
+    (Ruby323) => {
+        impl_ruby_version_with_module!(Ruby323, ruby_3_2_3);
+    };
+    (Ruby324) => {
+        impl_ruby_version_with_module!(Ruby324, ruby_3_2_4);
+    };
+    (Ruby325) => {
+        impl_ruby_version_with_module!(Ruby325, ruby_3_2_5);
+    };
+    (Ruby326) => {
+        impl_ruby_version_with_module!(Ruby326, ruby_3_2_6);
+    };
+    (Ruby327) => {
+        impl_ruby_version_with_module!(Ruby327, ruby_3_2_7);
+    };
+    (Ruby328) => {
+        impl_ruby_version_with_module!(Ruby328, ruby_3_2_8);
+    };
 
-    impl_ruby_str_to_rust_str!(rbspy_ruby_structs::ruby_3_1_0::RString);
+    // Ruby 3.3.x
+    (Ruby330) => {
+        impl_ruby_version_with_module!(Ruby330, ruby_3_3_0);
+    };
+    (Ruby331) => {
+        impl_ruby_version_with_module!(Ruby331, ruby_3_3_1);
+    };
+    (Ruby332) => {
+        impl_ruby_version_with_module!(Ruby332, ruby_3_3_2);
+    };
+    (Ruby333) => {
+        impl_ruby_version_with_module!(Ruby333, ruby_3_3_3);
+    };
+    (Ruby334) => {
+        impl_ruby_version_with_module!(Ruby334, ruby_3_3_4);
+    };
+    (Ruby335) => {
+        impl_ruby_version_with_module!(Ruby335, ruby_3_3_5);
+    };
+    (Ruby336) => {
+        impl_ruby_version_with_module!(Ruby336, ruby_3_3_6);
+    };
+    (Ruby337) => {
+        impl_ruby_version_with_module!(Ruby337, ruby_3_3_7);
+    };
+    (Ruby338) => {
+        impl_ruby_version_with_module!(Ruby338, ruby_3_3_8);
+    };
 }
 
-pub struct Ruby315;
+// Helper macro that does the actual implementation
+macro_rules! impl_ruby_version_with_module {
+    ($struct_name:ident, $module:ident) => {
+        pub struct $struct_name;
 
-impl RubyApiCompat for Ruby315 {
-    impl_iseq_functions!(rbspy_ruby_structs::ruby_3_1_5::rb_iseq_struct);
-    impl_thread_functions!(rbspy_ruby_structs::ruby_3_1_5::rb_thread_t);
-    impl_control_frame_functions!(
-        rbspy_ruby_structs::ruby_3_1_5::rb_control_frame_struct,
-        rbspy_ruby_structs::ruby_3_1_5::rb_execution_context_struct
-    );
-    impl_ruby_str_to_rust_str!(rbspy_ruby_structs::ruby_3_1_5::RString);
+        impl RubyApiCompat for $struct_name {
+            impl_iseq_functions!(rbspy_ruby_structs::$module::rb_iseq_struct);
+            impl_thread_functions!(rbspy_ruby_structs::$module::rb_thread_t);
+            impl_control_frame_functions!(
+                rbspy_ruby_structs::$module::rb_control_frame_struct,
+                rbspy_ruby_structs::$module::rb_execution_context_struct
+            );
+            impl_ruby_str_to_rust_str!(rbspy_ruby_structs::$module::RString);
+        }
+    };
 }
 
-pub struct Ruby320;
+// Ruby 3.1.x implementations
+impl_ruby_version!(Ruby310);
+impl_ruby_version!(Ruby311);
+impl_ruby_version!(Ruby312);
+impl_ruby_version!(Ruby313);
+impl_ruby_version!(Ruby314);
+impl_ruby_version!(Ruby315);
+impl_ruby_version!(Ruby316);
+impl_ruby_version!(Ruby317);
 
-impl RubyApiCompat for Ruby320 {
-    impl_iseq_functions!(rbspy_ruby_structs::ruby_3_2_0::rb_iseq_struct);
-    impl_thread_functions!(rbspy_ruby_structs::ruby_3_2_0::rb_thread_t);
-    impl_control_frame_functions!(
-        rbspy_ruby_structs::ruby_3_2_0::rb_control_frame_struct,
-        rbspy_ruby_structs::ruby_3_2_0::rb_execution_context_struct
-    );
+// Ruby 3.2.x implementations
+impl_ruby_version!(Ruby320);
+impl_ruby_version!(Ruby321);
+impl_ruby_version!(Ruby322);
+impl_ruby_version!(Ruby323);
+impl_ruby_version!(Ruby324);
+impl_ruby_version!(Ruby325);
+impl_ruby_version!(Ruby326);
+impl_ruby_version!(Ruby327);
+impl_ruby_version!(Ruby328);
 
-    impl_ruby_str_to_rust_str!(rbspy_ruby_structs::ruby_3_2_0::RString);
-}
-
-pub struct Ruby322;
-
-impl RubyApiCompat for Ruby322 {
-    impl_iseq_functions!(rbspy_ruby_structs::ruby_3_2_5::rb_iseq_struct);
-    impl_thread_functions!(rbspy_ruby_structs::ruby_3_2_5::rb_thread_t);
-    impl_control_frame_functions!(
-        rbspy_ruby_structs::ruby_3_2_5::rb_control_frame_struct,
-        rbspy_ruby_structs::ruby_3_2_5::rb_execution_context_struct
-    );
-
-    impl_ruby_str_to_rust_str!(rbspy_ruby_structs::ruby_3_2_5::RString);
-}
-
-pub struct Ruby330;
-
-impl RubyApiCompat for Ruby330 {
-    impl_iseq_functions!(rbspy_ruby_structs::ruby_3_3_1::rb_iseq_struct);
-    impl_thread_functions!(rbspy_ruby_structs::ruby_3_3_1::rb_thread_t);
-    impl_control_frame_functions!(
-        rbspy_ruby_structs::ruby_3_3_1::rb_control_frame_struct,
-        rbspy_ruby_structs::ruby_3_3_1::rb_execution_context_struct
-    );
-
-    impl_ruby_str_to_rust_str!(rbspy_ruby_structs::ruby_3_3_1::RString);
-}
+// Ruby 3.3.x implementations
+impl_ruby_version!(Ruby330);
+impl_ruby_version!(Ruby331);
+impl_ruby_version!(Ruby332);
+impl_ruby_version!(Ruby333);
+impl_ruby_version!(Ruby334);
+impl_ruby_version!(Ruby335);
+impl_ruby_version!(Ruby336);
+impl_ruby_version!(Ruby337);
+impl_ruby_version!(Ruby338);
 
 // Main API struct
 pub struct RubyAPI {
@@ -246,11 +344,37 @@ pub struct RubyAPI {
 impl RubyAPI {
     pub fn new(version: RubyVersion) -> Self {
         let inner: Box<dyn RubyApiCompat> = match version {
+            // Ruby 3.1.x
             RubyVersion::Ruby310 => Box::new(Ruby310),
+            RubyVersion::Ruby311 => Box::new(Ruby311),
+            RubyVersion::Ruby312 => Box::new(Ruby312),
+            RubyVersion::Ruby313 => Box::new(Ruby313),
+            RubyVersion::Ruby314 => Box::new(Ruby314),
             RubyVersion::Ruby315 => Box::new(Ruby315),
+            RubyVersion::Ruby316 => Box::new(Ruby316),
+            RubyVersion::Ruby317 => Box::new(Ruby317),
+
+            // Ruby 3.2.x
             RubyVersion::Ruby320 => Box::new(Ruby320),
+            RubyVersion::Ruby321 => Box::new(Ruby321),
             RubyVersion::Ruby322 => Box::new(Ruby322),
+            RubyVersion::Ruby323 => Box::new(Ruby323),
+            RubyVersion::Ruby324 => Box::new(Ruby324),
+            RubyVersion::Ruby325 => Box::new(Ruby325),
+            RubyVersion::Ruby326 => Box::new(Ruby326),
+            RubyVersion::Ruby327 => Box::new(Ruby327),
+            RubyVersion::Ruby328 => Box::new(Ruby328),
+
+            // Ruby 3.3.x
             RubyVersion::Ruby330 => Box::new(Ruby330),
+            RubyVersion::Ruby331 => Box::new(Ruby331),
+            RubyVersion::Ruby332 => Box::new(Ruby332),
+            RubyVersion::Ruby333 => Box::new(Ruby333),
+            RubyVersion::Ruby334 => Box::new(Ruby334),
+            RubyVersion::Ruby335 => Box::new(Ruby335),
+            RubyVersion::Ruby336 => Box::new(Ruby336),
+            RubyVersion::Ruby337 => Box::new(Ruby337),
+            RubyVersion::Ruby338 => Box::new(Ruby338),
         };
 
         RubyAPI { inner }
@@ -299,16 +423,63 @@ pub fn detect_ruby_version() -> RubyVersion {
     unsafe {
         let version_str = get_ruby_version_string();
 
+        // Ruby 3.1.x
         if version_str.starts_with("3.1.0") {
             RubyVersion::Ruby310
+        } else if version_str.starts_with("3.1.1") {
+            RubyVersion::Ruby311
+        } else if version_str.starts_with("3.1.2") {
+            RubyVersion::Ruby312
+        } else if version_str.starts_with("3.1.3") {
+            RubyVersion::Ruby313
+        } else if version_str.starts_with("3.1.4") {
+            RubyVersion::Ruby314
         } else if version_str.starts_with("3.1.5") {
             RubyVersion::Ruby315
+        } else if version_str.starts_with("3.1.6") {
+            RubyVersion::Ruby316
+        } else if version_str.starts_with("3.1.7") {
+            RubyVersion::Ruby317
+
+        // Ruby 3.2.x
         } else if version_str.starts_with("3.2.0") {
             RubyVersion::Ruby320
+        } else if version_str.starts_with("3.2.1") {
+            RubyVersion::Ruby321
         } else if version_str.starts_with("3.2.2") {
             RubyVersion::Ruby322
+        } else if version_str.starts_with("3.2.3") {
+            RubyVersion::Ruby323
+        } else if version_str.starts_with("3.2.4") {
+            RubyVersion::Ruby324
+        } else if version_str.starts_with("3.2.5") {
+            RubyVersion::Ruby325
+        } else if version_str.starts_with("3.2.6") {
+            RubyVersion::Ruby326
+        } else if version_str.starts_with("3.2.7") {
+            RubyVersion::Ruby327
+        } else if version_str.starts_with("3.2.8") {
+            RubyVersion::Ruby328
+
+        // Ruby 3.3.x
         } else if version_str.starts_with("3.3.0") {
             RubyVersion::Ruby330
+        } else if version_str.starts_with("3.3.1") {
+            RubyVersion::Ruby331
+        } else if version_str.starts_with("3.3.2") {
+            RubyVersion::Ruby332
+        } else if version_str.starts_with("3.3.3") {
+            RubyVersion::Ruby333
+        } else if version_str.starts_with("3.3.4") {
+            RubyVersion::Ruby334
+        } else if version_str.starts_with("3.3.5") {
+            RubyVersion::Ruby335
+        } else if version_str.starts_with("3.3.6") {
+            RubyVersion::Ruby336
+        } else if version_str.starts_with("3.3.7") {
+            RubyVersion::Ruby337
+        } else if version_str.starts_with("3.3.8") {
+            RubyVersion::Ruby338
         } else {
             panic!("Unknown Ruby version: {}", version_str);
         }
