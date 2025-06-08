666fcd9bc6acf1abfbee7f0234dd620dc3bd822c
Mike Yang <yfractal@gmail.com>
2025-06-08 10:44:00 +0800

    feat: add test for iseqs_from_ec

--- ext/sdb/src/tester.rs
+++ ext/sdb/src/tester.rs
@@ -1,6 +1,19 @@
-use rb_sys::{VALUE, rb_int2inum};
+use rb_sys::{VALUE, rb_int2inum, rb_num2long, rb_ary_new, rb_ary_push};
+use libc::{c_char, c_int, c_long, c_void};
+
 
 pub(crate) unsafe extern "C" fn rb_get_ec_from_thread(_module: VALUE, thread: VALUE) -> VALUE {
     let ec = crate::stack_scanner::RUBY_API.get_ec_from_thread(thread) as isize;
     rb_int2inum(ec)
+}
+
+pub(crate) unsafe extern "C" fn rb_get_iseqs(_module: VALUE, ec_val: VALUE) -> VALUE {
+    let ec = rb_num2long(ec_val) as *const c_void as u64;
+    let array = rb_ary_new();
+
+    crate::stack_scanner::RUBY_API.iterate_frame_iseqs(ec, &mut |iseq_addr| {
+        rb_ary_push(array, rb_int2inum(iseq_addr as isize));
+    });
+
+    array
 }
--- spec/ruby_version_spec.rb
+++ spec/ruby_version_spec.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
+def foo
+  bar
+end
+
+def bar
+  sleep 1_000_000
+end
 RSpec.describe 'RubyVersion' do
   it 'Get execution context from thread' do
-    thread = Thread.new { sleep 10000000 }
+    thread = Thread.new { sleep 1_000_000 }
     ec = SdbTester.ec_from_thread(thread)
+    thread.kill
     expect(ec).not_to eq nil
+  end
+
+  describe 'Get iseqs from execution context' do
+    it 'Get iseqs from execution context' do
+      thread = Thread.new { foo }
+      ec = SdbTester.ec_from_thread(thread)
+      sleep 0.1
+      iseqs = SdbTester.iseqs_from_ec(ec)
+      expect(iseqs.count).to be >= 2
+      thread.kill
+    end
   end
 end
